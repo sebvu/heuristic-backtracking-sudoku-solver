603b12b923c98119ba3985608a686cf9150efecc
Jester <jester.sebastiansantos@gmail.com>
2026-03-17 04:57:02 -0500

    Create basic main.py file

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+# 81 squares, split into 9 blocks containing 9 squares in a 3x3 brid
+# each of the 9 blocks has to contain all numbers 1-9 in their squares
+# each vertical and horizontal squares must also contain 1-9 uniquely
+# every sudoku has "exactly" one correct solution
+class SudokuWorld:
+    def __init__(self):
+        print("hello world")
+
+h = SudokuWorld()
