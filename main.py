f887a7979fa1db1a9309a84ea176b851ad35b76a
Jester <jester.sebastiansantos@gmail.com>
2026-03-19 20:48:15 -0500

    Refactor project to reflect utilizing data.csv

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pandas as pd
 TERMINOLOGY:
     cell: a 3x3 grid
     sMap = the sudoku map
-    expRes = a dictionary containing a unique lookup ID, and the results (this is tentative)
+    expRes = dict to interpret experiment data
 """
 
 class SudokuWorld:
@@ -15,24 +15,19 @@ class SudokuWorld:
         self.X_COLS = 9
         self.Y_ROWS = 9
         self.sMap = [[0] * self.X_COLS] * self.Y_ROWS
-        self.expRes = { "isHeuristic": [bool],
+        self.expData = { "isHeuristic": [bool],
                         "solveTimeMs": [int],
                         "numOfOperations": [int],
                         "numOfBacktraces": [int],
                         "memUsage": [int] }
 
-    # Add a new experiment result with key ID
+    # # Add a new experiment result with key ID
     def __addExpRes(self, ID, res):
-        if self.__isIDInExpRes(ID): # secondary checks
-            print(f"ID {ID} already populated, failed to add to expRes.")
-            return
-        print(f"adding new ID {ID} and res")
-        self.expRes[ID] = res
         return
 
-    # Verify if ID is already in expRes
+    # # Verify if ID is already in expRes
     def __isIDInExpRes(self, ID) -> bool:
-        return True if ID in self.expRes else False # primary check
+        return False
 
     # verify position being checked before proceeding, will terminate program if not a valid position
     def __verifyPos(self, X_POS, Y_POS, funcName) -> bool:
@@ -89,93 +84,56 @@ class SudokuWorld:
 
     # Display specific results with corresponding ID
     def displayExpRes(self, ID):
-
-        if not self.__isIDInExpRes(ID):
-            print(f"ID {ID} is not in expRes, will not display anything")
-            return
-
-        print(f"displaying result for {ID} below") # will be implemented via tkinter
-        print(self.expRes[ID]) # temporary method
+        return
 
     # Display ALL results
     def expResSummary(self):
-        print("displays experiment summary") # will be implemented via tkinter
-        print(self.expRes) # temporary method
+        return
 
     # Clear ALL experiment results
     def clearExpRes(self):
-        self.expRes.clear()
+        return
 
     """
     NOTE TO CONTRIBUTORS:
     
     below is the section yall should mainly be focusing on, i.e. the brute force and heuristics function
 
-    in each one, you will be making a call to ID, and case val (refer to cases/ folder for names), make sure to put the
-    FULL file name ex. bruteForceSolve(1, "case1.txt"). ID must be unique
+    please if you got questions ask away yallsies
     """
 
     # Brute force solve experiment
-    def bruteForceSolve(self, ID, case):
-        if self.__isIDInExpRes(ID):
-            print(f"{ID} is not unique. terminate brute force solver for case {case}")
-            return
-
+    def bruteForceSolve(self, question, answer):
         """
         implement brute force solver here
-        res should contain in itself some datastructure, maybe a dict as well, that stores the following:
-        - solve time
-        - number of operations
-        - number of explored nodes
-        - number of backtracks
-        - memory usage (tentative)
+        do not 'interpret' results, just fill in new entries for each of these (MUST FILL FOR ALL OF THEM)
+        self.expRes = { "isHeuristic": [bool], (this will be False for brute force)
+                        "solveTimeMs": [int],
+                        "numOfOperations": [int],
+                        "numOfBacktraces": [int],
+                        "memUsage": [int] }
         """
-
-        print(f"brute force solve case {case}, ID {ID}")
-        res = f"results for case {case} ID {ID} brute force"
-        self.__addExpRes(ID, res)
 
     # Heuristics solve experiment
-    def heuristicsSolve(self, ID, case):
-        if self.__isIDInExpRes(ID):
-            print(f"{ID} is not unique. terminate heuristics solver for case {case}")
-            return
-
+    def heuristicsSolve(self, question, answer):
         """
         implement heuristics solver solver here
-        res should contain in itself some datastructure, maybe a dict as well, that stores the following:
-        - solve time
-        - number of operations
-        - number of explored nodes
-        - number of backtracks
-        - memory usage (tentative)
+        do not 'interpret' results, just fill in new entries for each of these (MUST FILL FOR ALL OF THEM)
+        self.expRes = { "isHeuristic": [bool], (this will be True for heuristics)
+                        "solveTimeMs": [int],
+                        "numOfOperations": [int],
+                        "numOfBacktraces": [int],
+                        "memUsage": [int] }
         """
-
-        print(f"heuristics solve case {case}, ID {ID}")
-        res = f"results for case {case} ID {ID} heuristics"
-        self.__addExpRes(ID, res)
 
 def main():
     s = SudokuWorld()
-    
-    s.bruteForceSolve(1, "1")
-    s.heuristicsSolve(2, "2")
-    s.heuristicsSolve(2, "67") # repeat
-
-    s.displayExpRes(1)
-    s.displayExpRes(2)
-    s.displayExpRes(3)
 
     print(s.sMap)
 
-    df = pd.read_csv("cases/test.csv")
+    df = pd.read_csv("cases/test.csv", usecols=["question", "answer"])
     
-    # print(df.iloc[:1000])
-    # print(df.head())
-    # print(df.info())
-    # print(df.describe())
-    print(df.columns)
-    # print(df.index)
+    print(df.to_string())
 
 # main func
 if __name__=="__main__":
