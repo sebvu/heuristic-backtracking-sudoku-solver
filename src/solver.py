dbc731094926d648f59f72f17f4f1256b186668d
Versaji1500 <joelsaji867@gmail.com>
2026-03-24 17:19:13 -0500

    added function descriptions

--- src/solver.py
+++ src/solver.py
@@ -20,15 +20,19 @@ Functions:
 """
 
 class SudokuSolver:
+    
+    # Init with world, and also initialize numOfOps and numOfBtraces to 0
     def __init__(self, world: SudokuWorld):
         self.world = world
         self.numOfOps = 0
         self.numOfBtraces = 0
 
+    # Helper function to reset numOfOps and numOfBtraces to 0 before each solve
     def _resetMetrics(self):
         self.numOfOps = 0
         self.numOfBtraces = 0
 
+    # Helper function to check if a number can be placed in a given position according to sudoku rules
     def _isValid(self, row, col, num):
         for x in range(self.world.X_COLS):
             if self.world.sMap[row][x] == num:
@@ -47,6 +51,8 @@ class SudokuSolver:
 
         return True
 
+    # Helper function to find the next empty cell in the sudoku board, returns (row, col)
+    # or None if no empty cells
     def _findNextEmpty(self):
         for y in range(self.world.Y_ROWS):
             for x in range(self.world.X_COLS):
@@ -55,6 +61,7 @@ class SudokuSolver:
 
         return None
 
+    # Backtracking algorithm for uninformed solve
     def _backtrackUninformed(self):
         pos = self._findNextEmpty()
         if pos is None:
@@ -74,6 +81,7 @@ class SudokuSolver:
 
         return False
 
+    # Helper function to record experiment data into world.expData
     def _recordExperiment(self, isHeuristic, startTime, numOfOps, numOfBtraces):
         peak = tracemalloc.get_traced_memory()[1] if tracemalloc.is_tracing() else 0
         peakInMB = peak / 1024 / 1024
@@ -115,6 +123,3 @@ class SudokuSolver:
             print("do something")
 
         self._recordExperiment(True, sTime, self.numOfOps, self.numOfBtraces)
-
-
-SudokuSolve = SudokuSolver
